88c5def5e64208440b30631d0195d9ea3af1103c
Kosto1221 <ostolany@gamil.com>
2024-08-24 17:00:56 +0900

    [Feat] Add Database function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from extractors.web3 import extract_web3_jobs
 
 app = Flask("JobScrapper")
 
+db = {}
+
 @app.route("/")
 def home():
     return render_template("home.html", name="nico")
@@ -11,9 +13,13 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    berlin = extarct_berlin_jobs(keyword)
-    web3 = extract_web3_jobs(keyword)
-    jobs = berlin + web3
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        berlin = extarct_berlin_jobs(keyword)
+        web3 = extract_web3_jobs(keyword)
+        jobs = berlin + web3
+        db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
 
 app.run("0.0.0.0")
