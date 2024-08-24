f5cbf6745fd3d564e2b9286b5997b3dc5c7f889a
Kosto1221 <ostolany@gamil.com>
2024-08-24 16:00:44 +0900

    [Feat] Build html structure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from extractors.berlin import extarct_berlin_jobs
+from extractors.web3 import extract_web3_jobs
 
 app = Flask("JobScrapper")
 
@@ -6,9 +8,13 @@ app = Flask("JobScrapper")
 def home():
     return render_template("home.html", name="nico")
 
-@app.route("/hello")
-def hello():
-    return 'hello!'
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    berlin = extarct_berlin_jobs(keyword)
+    web3 = extract_web3_jobs(keyword)
+    jobs = berlin + web3
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
 app.run("0.0.0.0")
 
